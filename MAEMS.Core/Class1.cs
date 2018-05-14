a04a432cef7b5cd55300a2e31e19aacde06b19bd
brien <brien@artofmars.org>
2018-05-13 20:42:31 -0700

    initial commit

---
+++ MAEMS.Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAEMS.Core
+{
+    public class Class1
+    {
+    }
+}
